af7b10c96359f317cde57b91a234f8659886077d
Yuurin <byyuurin@gmail.com>
2021-12-07 15:40:45 +0800

    增加 typescript 排除規則

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -2,8 +2,9 @@ module.exports = {
   extends: ['@byyuurin/eslint-config-basic', 'plugin:@typescript-eslint/recommended'],
 
   rules: {
+    'no-undef': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'no-undef': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }

a9654a3cda0ba4a2897baf7874d5a04424d5b27a
Yuurin <byyuurin@gmail.com>
2024-03-30 20:52:26 +0800

    feat: generate types for built-in rules

--- scripts/rules-type-gen.ts
+++ scripts/rules-type-gen.ts
@@ -1,9 +1,17 @@
 import fs from 'node:fs/promises'
+import { builtinRules } from 'eslint/use-at-your-own-risk'
 import { flatConfigsToRulesDTS } from 'eslint-typegen/core'
 import { comments, formatters, ignores, imports, javascript, jsdoc, jsonc, markdown, node, stylistic, toml, typescript, unicorn, unocss, vue, yaml } from '../src/configs'
 import { combine } from '../src/utils'
 
+const builtin = {
+  plugins: {
+    '': { rules: Object.fromEntries(builtinRules.entries()) },
+  },
+}
+
 const configs = await combine(
+  builtin,
   comments(),
   formatters(),
   ignores(),

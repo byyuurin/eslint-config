65724f3559bebec42833d0edc0fc3c125f648b1c
Yuurin <byyuurin@gmail.com>
2025-08-17 10:13:22 +0800

    refactor: migrate to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [
@@ -6,4 +6,5 @@ export default defineConfig({
   ],
   shims: true,
   format: ['esm'],
+  exports: true,
 })

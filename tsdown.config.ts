19c21f99afe6672df1ec6f4989220db65c68807d
Yuurin <byyuurin@gmail.com>
2026-01-24 14:16:39 +0800

    feat!: require eslint v9 (#3)
    * feat!: drop cjs build
    
    * feat!: upgrade dependencies, require eslint v9
    
    * fix(unicorn): move no-instanceof-array to no-instanceof-builtins
    
    * refactor: migrate to tsdown
    
    * chore: update dependencies

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
   shims: true,
+  format: ['esm'],
+  exports: true,
 })
